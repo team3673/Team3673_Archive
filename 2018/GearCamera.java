ab36f2e1e44fe01f375936a90ee5ea5c21bc7ae5
AlexiaMaye <krazykitty3.14@gmail.com>
2017-03-12 15:08:50 -0700

    Update GearCamera.java

--- 2018/GearCamera.java
+++ 2018/GearCamera.java
@@ -1,6 +1,7 @@
 package org.usfirst.frc.team3673.AlexiaW2018.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.cscore.AxisCamera;
 import edu.wpi.first.wpilibj.CameraServer;
 
@@ -8,14 +9,18 @@ import edu.wpi.first.wpilibj.CameraServer;
  *
  */
 public class GearCamera extends Command {
-		AxisCamera gearCamera;
-	
+	public void gearCamInit() {	
+	AxisCamera gearCam = CameraServer.getInstance().addAxisCamera("aCam", "10.36.73.47");
+	}
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+
+    
+    public void gearCamStartCapture() {
+    	SmartDashboard.getString("aCam", "10.36.73.47");
+    }
+    public void gearCamStop() {
     	
-    public void gearCamera() {
-    		gearCamera = CameraServer.getInstance().addAxisCamera("gearCamera", "10.36.73.47");
-    		CameraServer.getInstance().getVideo(gearCamera);
     }
 
     // Called just before this Command runs the first time
